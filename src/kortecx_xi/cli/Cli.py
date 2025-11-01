3fac5eed52f90adbc51aca890c53595c289c8676
kortecx <kortecxhelp@gmail.com>
2025-11-01 13:03:50 +0530

    initial rusty setup

---
+++ src/kortecx_xi/cli/Cli.py
@@ -0,0 +1,22 @@
+import click
+from backend import sum_as_string
+
+@click.group(help="The Executable Intelligence Platform")
+def kx():
+    pass
+
+
+@click.command(help="Initiate kickoff")
+@click.option("--enable_backend",is_flag=True, default=False, help="Enables Rust backend")
+def start(enable_backend):
+    if enable_backend:
+        print("Rust Backend Enabled")
+    else:
+        print("Using Python Backend")
+        print(sum_as_string(1, 2))
+
+kx.add_command(start)
+
+
+def main() -> None:
+    kx()
